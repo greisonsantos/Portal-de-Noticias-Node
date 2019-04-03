f6e429679b594c326fcf1025f51cb2b2856b3d5a
greison <greisonsantos03@gmail.com>
2019-04-03 11:29:57 -0300

    consign for autoload routes

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 var app = require ('./config/server');
 
-var rotahome = require('./app/routes/home')(app);
-// rotahome(app);
-var rotanoticia = require('./app/routes/noticias')(app);
-// rotanoticia(app);
+// var rotahome = require('./app/routes/home')(app);
+// var rotanoticia = require('./app/routes/noticias')(app);
+// var rotaformadd = require('./app/routes/form_add_noticia')(app);
 
-var rotaformadd = require('./app/routes/form_add_noticia')(app);
-// rotaformadd(app);
 
 app.listen(3000, function(){
-
      console.log(" tudo certo");
 });
 
 //install mysql
 // npm install  mysql --save
+
+//consign autoload  de rotas views controller e models
+// npm install consign --save
--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
 var express = require('express');  //retorna uma função sem executar
-var app= express();
+var consign= require('consign'); //autoload automatico
 
+var app= express();
 app.set('view engine', 'ejs'); //passando para o express que o ejs será o motor de views
 app.set('views','./app/views')
+
+consign().include('app/routes').into(app);
+
 
 module.exports = app;
